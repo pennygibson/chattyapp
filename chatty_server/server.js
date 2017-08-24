e198202646d6d010ea18627c2244c5fb3262cadf
pennygibson <cenneboulet@yahoo.ca>
2017-08-24 17:24:12 -0400

    cleaned out unnecessary comments and added read me

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -16,7 +16,7 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
+// Set up a cllback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 let clients = {}
@@ -28,15 +28,9 @@ wss.on('connection', (client) => {
   //initialize a new client id
   const clientId = uuid()
   //send initial client data
-  // clientConnected(client, clientId);
-  //handle messages
+
   client.on('message', broadcastBack)
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  //client.on('close', () => {
-    //clientDisconnected(clientId
 
-
-  //});
   client.on('close', () => {
     console.log('Client disconnected');
     let userCount = wss.clients.size;
@@ -63,61 +57,10 @@ function broadcastBack(message) {
   }
 
 
-  // const response = Object.assign({
-  //   id: uuid(),
-  //   type: "incomingMessage"
-  // }, message)
-  // console.log(`Response: ${JSON.stringify(response)}`)
-  // wss.broadcast(response);
+
   wss.broadcast(message);
 }
-//connection event
-// function clientConnected(client, clientId){
-//   //create client data
-//   clients[clientId] = {
-//     id: clientId,
-//     name: //message or username
-//     connectedClients: clients
-//   }
-// }
-// //Setup message to be set to the client
-// //includes all currently connected clients
-// const setupMsg = {
-//   type: 'setup',
-//   data: {
-//     id: clientId,
-//     connectedClients: clients
-//   }
-// }
-// //connection message to be sent to the client
-// //tells the client who they are
-// const connectionMsg = {
-//   type: 'connection',
-//   data: clients[clientId]
-// }
 
-// if (client.readyState === client.OPEN) {
-//   client.send(JSON.stringify(setupMsg))
-// }
-// wss.broadcast(JSON.stringify(connectionMsg))
-// console.log(`>>${clients[clientId].name}`, clients[clientId])
-// }
-// //handles incoming messages
-// function handleMessages(incoming){
-//   //Broadcast message back no matter what
-//   wss.broadcast(incoming)
-//   var message = JSON.parse(incoming)
-
-//   switch(message.type) {
-//     case 'action':
-//     //update client state based on id
-//     clients[message.data.id] = clients[message.data]
-//     break
-
-//     default:
-//       console.log('Unsupported message: ', message)
-//   }
-// }
 
 
 
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,8 +1,5 @@
 import React, {Component} from 'react';
 
-
-
-
 class Nav extends Component {
 
   render(){
